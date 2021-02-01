0b84ddabc197589444fd358f78fbf738373a0b61
Pezhvak <pezhvakimv@gmail.com>
2021-02-01 22:56:44 +0330

    added variables

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -6,13 +6,15 @@ install-php-extensions @composer-${INSTALL_COMPOSER_VERSION}
 # Installing PHP Extensions
 docker-php-ext-configure opcache --enable-opcache &&
   docker-php-ext-configure gd --enable-gd --with-freetype --with-jpeg --with-webp &&
-  docker-php-ext-configure zip
-docker-php-ext-install -j "$(nproc)" \
-  mysqli \
-  pdo_mysql \
-  sockets \
-  intl \
-  bz2 \
-  pcntl \
-  bcmath \
-  gd
+  docker-php-ext-configure zip &&
+  docker-php-ext-install -j "$(nproc)" \
+    mysqli \
+    pdo_mysql \
+    sockets \
+    intl \
+    bz2 \
+    pcntl \
+    bcmath
+
+# note: for some reason if we build gd with the rest of the extensions it will trow an error in php -v
+docker-php-ext-install -j "$(nproc)" gd
