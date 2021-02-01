fbd66593a59f152ac522130372707e81813b88e4
Pezhvak <pezhvakimv@gmail.com>
2021-02-01 23:34:21 +0330

    added variables

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -16,9 +16,9 @@ apk add --update --no-cache \
   icu-dev
 
 # Installing PHP Security Checker
-PHP_SC_VERSION=$(curl -LSs "https://api.github.com/repos/fabpot/local-php-security-checker/releases/latest" |
+PHP_SC_VERSION=$(curl -s "https://api.github.com/repos/fabpot/local-php-security-checker/releases/latest" |
   grep '"tag_name":' |
   sed -E 's/.*"([^"]+)".*/\1/;s/^v//')
-curl -sS https://github.com/fabpot/local-php-security-checker/releases/download/v${PHP_SC_VERSION}/local-php-security-checker_${PHP_SC_VERSION}_linux_amd64 >/usr/local/bin/local-php-security-checker
+curl -LSs https://github.com/fabpot/local-php-security-checker/releases/download/v${PHP_SC_VERSION}/local-php-security-checker_${PHP_SC_VERSION}_linux_amd64 >/usr/local/bin/local-php-security-checker
 chmod +x /usr/local/bin/local-php-security-checker
 unset PHP_SC_VERSION
