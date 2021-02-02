063fd881792e3a7422b9fd10a175294da3183019
Pezhvak <pezhvakimv@gmail.com>
2021-02-03 01:21:39 +0330

    fixed an issue in installing reg

--- scripts/install-packages.sh
+++ scripts/install-packages.sh
@@ -31,7 +31,7 @@ REG_VERSION=$(curl -s "https://api.github.com/repos/genuinetools/reg/releases/la
   grep '"tag_name":' |
   sed -E 's/.*"([^"]+)".*/\1/;s/^v//')
 export REG_SHA256="ade837fc5224acd8c34732bf54a94f579b47851cc6a7fd5899a98386b782e228"
-$ curl -fSL "https://github.com/genuinetools/reg/releases/download/v${REG_VERSION}/reg-linux-amd64" -o "/usr/local/bin/reg" &&
+curl -fSL "https://github.com/genuinetools/reg/releases/download/v${REG_VERSION}/reg-linux-amd64" -o "/usr/local/bin/reg" &&
   echo "${REG_SHA256}  /usr/local/bin/reg" | sha256sum -c - &&
   chmod a+x "/usr/local/bin/reg"
 unset REG_SHA256 REG_VERSION
