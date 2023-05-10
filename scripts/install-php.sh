1199f21a2a9bbab846f902337c312a603c1be7ba
Pezhvak <pezhvakimv@gmail.com>
2023-05-10 17:06:22 +0330

    Update install-php.sh
    Fixed IMAP extension setup.

--- scripts/install-php.sh
+++ scripts/install-php.sh
@@ -7,22 +7,21 @@ install-php-extensions @composer-${COMPOSER_VERSION} imagick
 # Installing PHP Extensions
 docker-php-ext-configure opcache --enable-opcache
 docker-php-ext-configure zip --with-libzip
-docker-php-ext-configure imap --with-imap --with-imap-ssl
 docker-php-ext-install -j "$(nproc)" \
   sockets \
   bz2 \
   bcmath \
-  exif \
-  imap
+  exif
 
 docker-php-ext-install -j "$(nproc)" mysqli pdo pdo_mysql
 docker-php-ext-install -j "$(nproc)" zip
 install-php-extensions redis
 install-php-extensions intl
+install-php-extensions imap
 
-docker-php-ext-configure pcntl --enable-pcntl \
-&& docker-php-ext-install pcntl \
-&& docker-php-ext-enable pcntl
+docker-php-ext-configure pcntl --enable-pcntl &&
+  docker-php-ext-install pcntl &&
+  docker-php-ext-enable pcntl
 
 docker-php-ext-configure gd --enable-gd --with-freetype --with-jpeg --with-webp
 # note: for some reason if we build gd with the rest of the extensions it will trow an error in php -v
